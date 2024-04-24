468dba8890ef016297d108c0a707e248d562032a
jane3310 <167941949+jane3310@users.noreply.github.com>
2024-04-24 09:10:47 +0700

    Add files via upload

---
+++ Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Janeshop.Models
+{
+    public class Cart
+    {
+        public string product_id { get; set; }
+        public string product_name { get; set; }
+        public Nullable<double> price { get; set; }
+        public Nullable<int> qty { get; set; }
+        public Nullable<double> order_bill { get; set; }
+        public string store_id { get; set; }
+        public string store_name { get; set; }
+        //-------------------------------
+        public TBL_PRODUCT tbl_product { get; set; }
+        //-------------------------------
+        public Nullable<double> store_bill { get; set; }
+        public string status { get; set; }
+        public Nullable<double> order_price_sab { get; set; }
+        public Nullable<double> order_bill_sab { get; set; }
+    }
+}
---
+++ Models/CusRegister.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Janeshop.Models
+{
+    public class CusRegister
+    {
+        public string customer_id { get; set; }
+        [Required(ErrorMessage = "กรุณาระบุ เลือกคำนำหน้า")]
+        public string title_id { get; set; }
+        [Required(ErrorMessage = "กรุณาระบุ ชื่อ")]
+        public string fname { get; set; }
+        [Required(ErrorMessage = "กรุณาระบุ สกุล")]
+        public string lname { get; set; }
+        [Required(ErrorMessage = "กรุณาระบุ เบอร์โทรศัพท์")]
+        [MinLength(10, ErrorMessage = "กรุณาระบุ เบอร์โทรไม่น้อยกว่า 10 ตัว")]
+        [MaxLength(10, ErrorMessage = "กรุณาระบุ เบอร์โทรไม่เกิน 10 ตัว")]
+        public string mobile { get; set; }
+        [Required(ErrorMessage = "กรุณาระบุ รหัสผ่าน")]
+        [MinLength(4, ErrorMessage = "กรุณาระบุ รหัสผ่านไม่น้อยกว่า 4 ตัว")]
+        [MaxLength(10, ErrorMessage = "กรุณาระบุ รหัสผ่านไม่เกิน 10 ตัว")]
+        public string password { get; set; }
+        public bool cus_status { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public string LoginErrorMessage { get; internal set; }
+        [NotMapped]
+        [Required(ErrorMessage = "กรุณาระบุ รหัสผ่าน")]
+        [CompareAttribute("password", ErrorMessage = "รหัสผ่านไม่ตรงกัน")]
+        public string Confirmpassword { get; set; }
+    }
+}
